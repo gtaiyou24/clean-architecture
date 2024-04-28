8b2f19670e95ed4d51b2cb78d3aa7fbbf217f984
gtaiyou24 <gtaiyou24@gmail.com>
2024-04-29 01:03:35 +0900

    style: @override

--- app/port/adapter/persistence/repository/inmem/in_mem_user_repository.py
+++ app/port/adapter/persistence/repository/inmem/in_mem_user_repository.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import override
+
 from domain.model.mail import EmailAddress
 from domain.model.user import UserRepository, User
 
@@ -8,15 +10,18 @@ class InMemUserRepository(UserRepository):
     def __init__(self):
         self.users: set[User] = set()
 
+    @override
     def add(self, user: User) -> None:
         self.users.add(user)
 
+    @override
     def user_with_email_address(self, email_address: EmailAddress) -> User | None:
         for user in self.users:
             if user.email_address == email_address:
                 return user
         return None
 
+    @override
     def user_with_token(self, value: str) -> User | None:
         for user in self.users:
             if user.token_with(value) is not None:
--- app/port/adapter/persistence/repository/mysql/user/mysql_user_repository.py
+++ app/port/adapter/persistence/repository/mysql/user/mysql_user_repository.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from injector import inject
+from typing import override
 
 from domain.model.mail import EmailAddress
 from domain.model.user import UserRepository, User
@@ -13,11 +14,14 @@ class MySQLUserRepository(UserRepository):
     def __init__(self, cache_layer_user: CacheLayerUser):
         self.__cache_layer_user = cache_layer_user
 
+    @override
     def add(self, user: User) -> None:
         self.__cache_layer_user.set(user)
 
+    @override
     def user_with_email_address(self, email_address: EmailAddress) -> User | None:
         return self.__cache_layer_user.user_or_origin(email_address)
 
+    @override
     def user_with_token(self, value: str) -> User | None:
         return self.__cache_layer_user.user_or_origin_with_token(value)
