e559b69dcf9ae61e1c74fcc85954d0b1ddec9d0a
gtaiyou24 <gtaiyou24@gmail.com>
2024-04-29 22:57:02 +0900

    fix: bin/rename.sh

--- bin/rename.sh
+++ bin/rename.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 repo_name=$1
+export LANG=C
 
-sed -i '' "s/clean-architecture/${repo_name}/g" *.py
-sed -i '' "s/clean-architecture/${repo_name}/g" *.yaml
-
+grep 'clean-architecture' -rl * --exclude-dir={venv,bin,doc} --exclude=README.md | xargs sed -i '' "s/clean-architecture/${repo_name}/g"
 grep 'clean-architecture' -rl .* --exclude-dir={.idea,.git} | xargs sed -i '' "s/clean-architecture/${repo_name}/g"
