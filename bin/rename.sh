1483d732a4670373cf1306bcc35300f60fbcf1b6
taiyo tamura <gtaiyou24@users.noreply.github.com>
2024-05-03 04:16:32 +0900

    Update rename.sh

--- bin/rename.sh
+++ bin/rename.sh
@@ -4,4 +4,4 @@ repo_name=$1
 export LANG=C
 
 sed -i '' "s/clean-architecture/${repo_name}/g" app/app.py
-grep 'clean-architecture' -rl .* --exclude-dir={.idea,.git} | xargs sed -i '' "s/clean-architecture/${repo_name}/g"
+grep 'clean-architecture' -rl .* --exclude=README.md --exclude-dir={.idea,.git,bin} | xargs sed -i '' "s/clean-architecture/${repo_name}/g"
