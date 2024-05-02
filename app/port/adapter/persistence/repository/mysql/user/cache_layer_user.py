bfda1ce48632b1e7fb62f6290fefbb532365815f
taiyo tamura <gtaiyou24@users.noreply.github.com>
2024-05-03 05:48:19 +0900

    Update cache_layer_user.py

--- app/port/adapter/persistence/repository/mysql/user/cache_layer_user.py
+++ app/port/adapter/persistence/repository/mysql/user/cache_layer_user.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from cachetools import TTLCache, cached
 from injector import singleton, inject
 
 from domain.model.mail import EmailAddress
@@ -11,6 +10,7 @@ from port.adapter.persistence.repository.mysql.user import DriverManagerUser
 @singleton
 class CacheLayerUser:
     """キャッシュを保持するクラス"""
+    values = dict()
 
     # 60秒 × 15分
     __TTL = 60 * 15
@@ -19,18 +19,34 @@ class CacheLayerUser:
     def __init__(self, driver_manager_user: DriverManagerUser):
         self.__driver_manager_user = driver_manager_user
 
-    @cached(cache=TTLCache(maxsize=128, ttl=__TTL))
     def user_or_origin(self, email_address: EmailAddress) -> User | None:
-        return self.__driver_manager_user.find_by_email_address(email_address)
+        key = f'email_address-{email_address.value}'
+        if key in self.values.keys():
+            return self.values[key]
 
-    @cached(cache=TTLCache(maxsize=128, ttl=__TTL))
+        optional = self.__driver_manager_user.find_by_email_address(email_address)
+        self.values[key] = optional
+        return self.values[key]
+
     def user_or_origin_with_token(self, token: str) -> User | None:
-        return self.__driver_manager_user.find_by_token(token)
+        key = f'token-{token}'
+        if key in self.values.keys():
+            return self.values[key]
+
+        optional = self.__driver_manager_user.find_by_token(token)
+        self.values[key] = optional
+        return self.values[key]
 
     def set(self, user: User) -> None:
         self.__driver_manager_user.upsert(user)
-        TTLCache(maxsize=128, ttl=self.__TTL).clear()
+        # キャッシュを更新する
+        self.values[f'email_address-{user.email_address.value}'] = user
+        for token in user.tokens:
+            self.values[f'token-{token.value}'] = user
 
     def delete(self, user: User) -> None:
         self.__driver_manager_user.delete(user)
-        TTLCache(maxsize=128, ttl=self.__TTL).clear()
+        # キャッシュを更新する
+        self.values[f'email_address-{user.email_address.value}'] = None
+        for token in user.tokens:
+            self.values[f'token-{token.value}'] = None
